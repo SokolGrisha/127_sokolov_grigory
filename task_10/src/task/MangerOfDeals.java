e8bf9e45f5fde71b3235a67f0d955350fd236ae3
SokolGrisha <grsha9@gmail.com>
2022-01-11 00:53:49 +0300

    task 10.13 rewrite backend

---
+++ task_10/src/task/MangerOfDeals.java
@@ -0,0 +1,14 @@
+package task;
+
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Scanner;
+import static task.TypeDoc.*;
+
+public class MangerOfDeals {
+    private HashMap<String, Deal> deals;
+    public MangerOfDeals(){
+        deals = new HashMap<>();
+    }
+
+}
